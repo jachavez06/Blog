703901688aaf71574c539c6e85fd57752df43af4
Jesus Chavez <jachavez06@gmail.com>
2017-07-12 02:09:10 -0500

    Articles new and create actions complete. Adding errors messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,19 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end 
 
-    def create
-    end
-
     def new
+        @article = Article.new
     end 
+    
+    def create
+        @article = Article.new(articles_params)
+        if @article.save
+		    flash[:success] = "Article was successfully created!"
+		    redirect_to article_path(@article) 
+		else
+		    render 'new'
+        end 
+    end
 
     def edit
     end 
@@ -20,5 +28,11 @@ class ArticlesController < ApplicationController
     end 
 
     def destroy
+    end
+
+    private
+
+    def articles_params
+        params.require(:article).permit(:title, :content)
     end
 end
