d1cf6aa15f536d23243819c17c51155cdd82f38d
Jesus Chavez <jachavez06@gmail.com>
2017-07-28 16:42:12 -0500

    Add slug method to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:3, maximum:50}
     validates :content, presence:true, length: {minimum: 10} 
 
+    def slug
+    
+    end 
+    
     def to_param
 
     end 
