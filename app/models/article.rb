9f8dfb7638e686408ca0032688bb22280d5fdbe4
Jesus Chavez <jachavez06@gmail.com>
2017-07-28 16:39:15 -0500

    Override Article to_param

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:3, maximum:50}
     validates :content, presence:true, length: {minimum: 10} 
+
+    def to_param
+
+    end 
 end
