eaeca4b0e059bb7b2709a22db86ff9a7236d7fbe
Jesus Chavez <jachavez06@gmail.com>
2017-07-29 13:51:27 -0500

    Add session paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  root 'welcome#index'
+  mount Ckeditor::Engine => '/ckeditor' # Articles
+  root 'welcome#index'  
   resources :articles 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
