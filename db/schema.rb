94a707046706d6c6a6c5b50ef213b5329f3fb1fa
Jesus Chavez <jachavez06@gmail.com>
2017-07-29 13:41:17 -0500

    Add password to Users. Make admin field default to false for users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728214610) do
+ActiveRecord::Schema.define(version: 20170729183628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20170728214610) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
