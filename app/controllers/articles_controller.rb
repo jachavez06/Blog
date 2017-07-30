9332378eb719c5514692ccb3e56bd5e235afcaf6
Jesus Chavez <jachavez06@gmail.com>
2017-07-29 21:09:49 -0500

    Default and non-article meta-data added.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.paginate(page: params[:page], per_page: 1)
+        set_meta_tags description: 'A list of all the tutorials I have written.'
+        set_meta_tags keywords: 'Blog, Code, Tutorial, Guide, Example, Program'
     end 
 
     def new
