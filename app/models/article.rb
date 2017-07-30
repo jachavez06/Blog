89da8f9fc4cc2ba2af3227720bdd205876a78bb0
Jesus Chavez <jachavez06@gmail.com>
2017-07-29 22:16:00 -0500

    Metadata validation lengths.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,13 @@
 class Article < ApplicationRecord
     # Body
-    validates :title, presence: true, :uniqueness => {:case_sensitive => false}, length: {minimum:3, maximum:50}
-    validates :content, presence: true, length: {minimum: 10} 
+    validates :title, presence: true, :uniqueness => {:case_sensitive => false}, length: {minimum: 10, maximum: 50}
+    validates :content, presence: true, length: {minimum: 500} 
     
     # Meta-data
     @keyword_regex = /\A([a-z]+)(,\s*[a-z]+)*\z/i
-    validates :meta_data_title, presence: true, :uniqueness => {:case_sensitive => false}
-    validates :meta_data_description, presence: true
-    validates :meta_data_keywords, presence: true, :format => { :with => @keyword_regex, :message => "Invalid keyword format." }
+    validates :meta_data_title, presence: true, :uniqueness => {:case_sensitive => false}, length: {minimum: 10} 
+    validates :meta_data_description, presence: true, length: {minimum: 160} 
+    validates :meta_data_keywords, presence: true, :format => { :with => @keyword_regex, :message => "Invalid keyword format." }, length: {minimum: 3} 
 
     after_create :update_slug
     before_update :assign_slug 
