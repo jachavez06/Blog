e0618ae8046ac72997086690f977d373c6edae44
Jesus Chavez <jachavez06@gmail.com>
2017-07-30 20:37:59 -0500

    Padding to css and no br

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,44 +3,6 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?, :is_admin?
   
-  #before_action :prepare_meta_tags, if: "request.get?"
-
-  def prepare_meta_tags(options={})
-    site_name   = "JChavez Code"
-    title       = [controller_name, action_name].join(" ")
-    description = "The CookiesHQ team creates web software for humans"
-    image       = options[:image] || "your-default-image-url"
-    current_url = request.url
-
-    # Let's prepare a nice set of defaults
-    defaults = {
-      site:        site_name,
-      title:       title,
-      image:       image,
-      description: description,
-      keywords:    %w[web software development mobile app],
-      twitter: {
-        site_name: site_name,
-        site: '@thecookieshq',
-        card: 'summary',
-        description: description,
-        image: image
-      },
-      og: {
-        url: current_url,
-        site_name: site_name,
-        title: title,
-        image: image,
-        description: description,
-        type: 'website'
-      }
-    }
-
-    options.reverse_merge!(defaults)
-
-    set_meta_tags options
-  end
-  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end 
