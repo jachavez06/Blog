541d4d7c500c52948fd1884d4b312a1f1c5eb126
Jesus Chavez <jachavez06@gmail.com>
2017-07-31 01:47:31 -0500

    Login modal finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,11 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            flash[:success] = "You have successfully logged in."
             redirect_to articles_path
         else
-            flash.now[:danger] = "You have entered an invalid Username/Password."
-            render 'new'
+            respond_to do |format|
+                format.js {render 'create'}
+            end
         end           
     end 
     
