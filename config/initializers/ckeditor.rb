e7d054a36de7a3d508bd03ed6524e7b612a968da
Jesus Chavez <jachavez06@gmail.com>
2017-08-03 22:01:03 -0500

    Test

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,1 @@
 require "ckeditor/orm/active_record"
-
-Ckeditor.setup do |config|
-  # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
-end
