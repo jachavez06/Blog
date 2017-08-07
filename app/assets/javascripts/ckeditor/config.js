02829430067842b134a9718c941e50f7ec689dc6
Jesus Chavez <jachavez06@gmail.com>
2017-08-06 22:53:06 -0500

    Allow html attributes to be preserved.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,6 @@
 CKEDITOR.editorConfig = function (config) {
+    config.allowedContent = true;
+    config.removeFormatAttributes = '';
     config.entities = false;
     config.height = 500; 
     config.extraPlugins = 'codemirror';
