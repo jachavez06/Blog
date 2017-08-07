c8d81ecf6ca408ff51dee18e9c7ac543cd6cd9c1
Jesus Chavez <jachavez06@gmail.com>
2017-08-06 20:22:23 -0500

    Assets

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
 
     config.exception_handler = {dev: true}
