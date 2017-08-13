fcb3264ae6fcd4d5c996331e4a44065dc06e3399
Jesus Chavez <jachavez06@gmail.com>
2017-08-12 22:41:08 -0500

    Add admin migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813032818) do
+ActiveRecord::Schema.define(version: 20170813033643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "login"
+    t.string "password_digest"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_admins_on_user_id"
+  end
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
