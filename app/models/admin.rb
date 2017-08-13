88dcfb553b5957cca0b490906cc0157a59537f3a
Jesus Chavez <jachavez06@gmail.com>
2017-08-12 22:43:43 -0500

    Create Admin model and controller.

---
+++ app/models/admin.rb
@@ -0,0 +1,4 @@
+# Admins of the site.
+class Admin < ApplicationRecord
+    has_secure_password
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 # Users of the site.
 class User < ApplicationRecord
-  has_secure_password :validations => false
   validates :ip, uniqueness: true
 end
