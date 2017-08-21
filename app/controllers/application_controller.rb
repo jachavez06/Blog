642e52bfe39d5118ea47fdd469671876ac8d903c
Jesus Chavez <jachavez06@gmail.com>
2017-08-21 09:32:05 -0500

    Add rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,13 @@ class ApplicationController < ActionController::Base
     session[:admin] == true
   end
 
+#  if Rails.env.production?
+#    unless Rails.application.config.consider_all_requests_local
+      rescue from Exception, with: :render_500
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from ActionController::UnknownAction, with: :render_404
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+#    end
+#  end
 end
