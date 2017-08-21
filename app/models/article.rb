7f850eb95e947bbdbbd8b637f21eebbb60502bc5
Jesus Chavez <jachavez06@gmail.com>
2017-08-21 08:35:26 -0500

    Add all_tags virtual attribute to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,15 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
   # Body
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :content, presence: true
