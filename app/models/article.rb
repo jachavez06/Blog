016ec02536d4d0aea73579119165dada98981e63
Jesus Chavez <jachavez06@gmail.com>
2017-08-21 09:07:02 -0500

    Styling tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  # Used when form is processed
   def all_tags=(names)
     self.tags = names.split(',').map do |name|
       Tag.where(name: name.strip).first_or_create!
@@ -11,7 +12,7 @@ class Article < ApplicationRecord
   end
 
   def all_tags
-    self.tags.map(&:name).join(', ')
+    self.tags.map(&:name).join('</span><span class="badge badge-pill badge-default"> ')
   end
 
   # Body
