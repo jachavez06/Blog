832b602a3dd4da0743ba7bc30a863edf264c92d2
Jesus Chavez <jachavez06@gmail.com>
2017-08-21 19:12:04 -0500

    fix typo used to test 500 error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    set_meta_tag description: 'A blog where I post code tutorials so that others can learn from my experience.'
+    set_meta_tags description: 'A blog where I post code tutorials so that others can learn from my experience.'
     set_meta_tags keywords: 'Blog, Code, Tutorial, Guide, Example, Program'
   end
 end
