5a415d563de2b739dff8b63b1813cfd8e9f40a32
Jesus Chavez <jachavez06@gmail.com>
2017-08-26 13:08:02 -0500

    Pagination and different route for article creation's cancel button depending on whether it is a new article or one being edited.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsController < ApplicationController
     def index
-        @unpublished = Article.where(published: false) if admin?
+        @unpublished = Article.where(published: false).paginate(:page => params[:page], :per_page => 1) if admin?
     end
 end
