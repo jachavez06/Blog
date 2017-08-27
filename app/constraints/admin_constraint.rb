578108906214d5565f01b63e0f82fe734a675d57
Jesus Chavez <jachavez06@gmail.com>
2017-08-27 14:22:36 -0500

    Remove byebugs.

--- app/constraints/admin_constraint.rb
+++ app/constraints/admin_constraint.rb
@@ -1,12 +1,10 @@
 class AdminConstraint
     def matches?(request)
-        byebug
       user = current_user(request)
       user.present? && request.session[:admin]
     end
   
     def current_user(request)
-        byebug
         User.find_by(ip: request.session[:ip])
     end
 end
