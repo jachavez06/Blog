5866ee749d53259685948e61f43e9aef9ba29be5
Jesus Chavez <jachavez06@gmail.com>
2017-08-30 14:04:32 -0500

    Commenting in index action of admins controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,8 @@
 # /app/controllers/admins_controller.rb
 class AdminsController < ApplicationController
   def index
-    @unpublished = Article.where(published: false).paginate(page: params[:page], per_page: 10) if admin?
-  end
+    # Paginate unpublished articles
+    @unpublished = Article.where(published: false).paginate(page: params[:page], 
+                                 per_page: 10) if admin?
+  en
 end
