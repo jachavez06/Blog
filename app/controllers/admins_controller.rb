c6d409c38298bc94ed9056488476803407f4d3df
Jesus Chavez <jachavez06@gmail.com>
2017-09-04 17:39:19 -0500

    Change ordering of articles (show newest first)

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,6 @@ class AdminsController < ApplicationController
   def index
     # Paginate unpublished articles
     @unpublished = Article.where(published: false).paginate(page: params[:page], 
-                                 per_page: 10) if admin?
+                                 per_page: 10).order(created_at: :desc) if admin?
   end
 end
