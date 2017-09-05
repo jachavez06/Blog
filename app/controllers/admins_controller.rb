17666206965fbef69b595bb26c329b3fa31dcb6e
Jesus Chavez <jachavez06@gmail.com>
2017-09-04 20:13:18 -0500

    Admin panel shows unpublshed articles

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,7 @@
 class AdminsController < ApplicationController
   def index
     # Paginate unpublished articles
-    @unpublished = Article.where(published: false).paginate(page: params[:page], 
+    @unpublished = Article.where(state: 0).paginate(page: params[:page], 
                                  per_page: 10).order(created_at: :desc) if admin?
   end
 end
