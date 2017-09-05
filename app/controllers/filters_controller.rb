69eb7b7af81ca6e704c809bbef9c4d6d7fb09692
Jesus Chavez <jachavez06@gmail.com>
2017-09-05 17:31:31 -0500

    Precompile.

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,5 +1,5 @@
 # Controls filtering by tags
-class TagsController < ApplicationController
+class FiltersController < ApplicationController
   def show
     @articles = Article.where(state: 1).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
   end
