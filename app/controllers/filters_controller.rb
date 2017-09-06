e23c1165953667f655a90eac281cfe659d799ec2
Jesus Chavez <jachavez06@gmail.com>
2017-09-05 21:47:53 -0500

    Now only tags that show up are those that have a published article attached to them.

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,6 +2,6 @@
 class FiltersController < ApplicationController
   def show
     @articles = Article.where(state: 1).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = @articles.tag_counts_on(:tags)
   end
 end
