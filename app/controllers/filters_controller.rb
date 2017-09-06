a70df9d7304904cfcd333949c1d1b23634528cb6
Jesus Chavez <jachavez06@gmail.com>
2017-09-05 21:31:26 -0500

    Add tags list to filters controller

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,5 +2,6 @@
 class FiltersController < ApplicationController
   def show
     @articles = Article.where(state: 1).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
+    @tags = ActsAsTaggableOn::Tag.all
   end
 end
