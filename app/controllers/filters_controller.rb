964aca3ca04eabfa794b2c9ab01fb04f7c148ef5
Jesus Chavez <jachavez06@gmail.com>
2017-09-07 13:53:22 -0500

    Create skeleton for action to be taken before tag search results are given.

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,7 +1,15 @@
 # Controls filtering by tags
 class FiltersController < ApplicationController
+  before_action :require_published
+
   def show
     @articles = Article.where(state: 1).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 10).order(created_at: :desc)
     @tags = @articles.tag_counts_on(:tags)
+  end
+
+  private
+
+  def require_published
+
   end
 end
