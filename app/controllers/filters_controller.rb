f12f4b4718bd9977c49c7bea7d75492d57643213
Jesus Chavez <jachavez06@gmail.com>
2017-09-08 09:53:56 -0500

    Comments

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -13,9 +13,10 @@ class FiltersController < ApplicationController
     # Save param so don't have to access it again
     @tag = params[:tag]
 
+    # Check if tag is published
     return if Rails.cache.read('published_tags').include? @tag
 
-    # If they reach this point, then they tried to reach a tag that has not been published or does not exist
+    # If here, then user tried to access unpublished or non-existent tag.
     redirect_to root_path
   end
 end
